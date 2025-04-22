c173b3a91fba232d983b10cb220ecd03c5a54544
aveA <aveA@AVE>
2025-04-22 21:40:19 +0300

    миграции

--- entityframewrok/EFPractices/Entities/Company.cs
+++ entityframewrok/EFPractices/Entities/Company.cs
@@ -3,7 +3,12 @@
     public class Company
     {
         public int Id { get; set; }
+        
         public string Name { get; set; }
+
+        public string City { get; set; }
+
+        public string Type { get; set; }
 
         public List<User> Users { get; set; } = new List<User>();
     }
--- entityframewrok/EFPractices/Entities/User.cs
+++ entityframewrok/EFPractices/Entities/User.cs
@@ -9,20 +9,20 @@
         public string Email { get; set; }
         public string Role { get; set; }
 
-        // Навигационное свойство
-        public UserCredential UserCredential { get; set; }
-
-        // Навигационное свойство
-        public List<Topic> Topics { get; set; } = new List<Topic>();
-
-        //Свойства CompanyId и Company в классе User, на самом деле, не являются обязательными
-        //для создания связи между таблицами и могут быть опущены, пока в классе Company остается поле Users
-        //Но явное указание данных свойств позволяет работать со связью объектов в обе стороны, поэтому советуем оставлять их.
-
         // Внешний ключ
         public int CompanyId { get; set; }
 
         // Навигационное свойство
         public Company Company { get; set; }
+
+        // Навигационное свойство
+        //public UserCredential UserCredential { get; set; }
+
+        // Навигационное свойство
+        //public List<Topic> Topics { get; set; } = new List<Topic>();
+
+        //Свойства CompanyId и Company в классе User, на самом деле, не являются обязательными
+        //для создания связи между таблицами и могут быть опущены, пока в классе Company остается поле Users
+        //Но явное указание данных свойств позволяет работать со связью объектов в обе стороны, поэтому советуем оставлять их.
     }
 }
